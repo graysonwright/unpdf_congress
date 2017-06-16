34fb82473e24bdc6df9a50ac1b3f6e13161bc3a2
Grayson Wright <wright.grayson@gmail.com>
2017-06-16 14:10:08 -0400

    Add some basic profiling information
    Output displays as:
    
    ```
    root@9670e9cd7967:/app# bundle exec ruby process.rb
    
    Processing examples/derivatives_tax_act.pdf...
            56 pages        completed in 1.636546 seconds
    
    Processing examples/equality_act.pdf...
            24 pages        completed in 0.775332 seconds
    
    Processing examples/flood_insurance.pdf...
            69 pages        completed in 2.285814 seconds
    
    Processing examples/megans_law.pdf...
            30 pages        completed in 1.077843 seconds
    
    Processing examples/north_korea_sanctions.pdf...
            62 pages        completed in 1.990806 seconds
    
    Processing examples/patient-protection.pdf...
            2409 pages      completed in 83.771238 seconds
    
    ```

--- process.rb
+++ process.rb
@@ -1,18 +1,28 @@
 require "pdf-reader"
+require "benchmark"
 
 INPUT_DIRECTORY = "examples"
 OUTPUT_DIRECTORY = "output"
 
 input_files = Dir.glob("#{INPUT_DIRECTORY}/*.pdf")
 
+puts
+
 input_files.each do |input_file|
-  pdf = PDF::Reader.new(input_file)
-
-  output = pdf.pages.map(&:text).join("\n")
-
   output_filename = input_file.
     gsub(INPUT_DIRECTORY, OUTPUT_DIRECTORY).
     gsub(/.pdf$/, ".txt")
 
-  File.write(output_filename, output)
+  puts "Processing #{input_file}..."
+
+  time = Benchmark.realtime do
+    pdf = PDF::Reader.new(input_file)
+    print "\t#{pdf.page_count} pages\t"
+    output = pdf.pages.map(&:text).join("\n")
+
+    File.write(output_filename, output)
+  end
+
+  puts "completed in #{'%2f' % time} seconds"
+  puts
 end
