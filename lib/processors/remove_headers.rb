a8971f362230d6f85e8be1a4b4e86e82c48f7a19
Grayson Wright <wright.grayson@gmail.com>
2017-06-16 16:09:27 -0400

    Improve header processor to get rid of page #s
    It makes sense to group this in with the header processor,
    because page numbers offten appear immediately following the header.
    Without grouping them,
    it would likely be difficult to detect page numbers accurately.

--- lib/processors/remove_headers.rb
+++ lib/processors/remove_headers.rb
@@ -3,20 +3,39 @@ require_relative "base"
 module Processors
   class RemoveHeaders < Base
     HEADER_FORMAT = /[A-Z]{3}\d{5}/
+    LINE_NUMBER_FORMAT = /^\s*\d+\s*$/
 
     def processed_text
-      header = text.lines.first
+      lines = text.lines
 
-      if header =~ HEADER_FORMAT
+      if !(lines.first =~ HEADER_FORMAT)
+        text
+      else
         # Construct a regular expression out of the header
         # to handle whitespace variations
-        header.gsub!(/ +/, '\s+')
+        header = lines.first.gsub(/ +/, '\s+')
 
-        text.lines.reject do |line|
-          line =~ /^#{header}$/
-        end.join
-      else
-        text
+        acceptable_lines = []
+
+        line_index = 0
+        while line_index < lines.count
+          if(lines[line_index] =~ /^#{header}$/)
+            # found a header!
+            # now let's look for a line number immediately following it
+
+            if(lines[line_index+1] =~ LINE_NUMBER_FORMAT)
+              # the next line only contains a line number.
+              # Jump ahead to skip it.
+              line_index += 1
+            end
+          else
+            acceptable_lines << lines[line_index]
+          end
+
+          line_index += 1
+        end
+
+        acceptable_lines.join
       end
     end
   end
--- spec/processors/remove_headers_spec.rb
+++ spec/processors/remove_headers_spec.rb
@@ -2,44 +2,39 @@ require "processors/remove_headers"
 
 describe Processors::RemoveHeaders do
   it "removes exact matches for the header" do
-    header = "FRA17032   S.L.C.\n"
-
-    document = <<-DOC
+    expect(Processors::RemoveHeaders.process(<<-DOC)).
       FRA17032   S.L.C.
       Some text
       FRA17032   S.L.C.
       Some more text
     DOC
-
-    expect(Processors::RemoveHeaders.process(document)).to eq(<<-DOC)
+    to eq(<<-DOC)
       Some text
       Some more text
     DOC
   end
 
   it "removes variations on the header with different whitespace" do
-    document = <<-DOC
+    expect(Processors::RemoveHeaders.process(<<-DOC)).
       FRA17032   S.L.C.
       Some text
       FRA17032      S.L.C.
       Some more text
     DOC
-
-    expect(Processors::RemoveHeaders.process(document)).to eq(<<-DOC)
+    to eq(<<-DOC)
       Some text
       Some more text
     DOC
   end
 
   it "does not remove lines with non-header content" do
-    document = <<-DOC
+    expect(Processors::RemoveHeaders.process(<<-DOC)).
       FRA17032   S.L.C.
       Some text
       Something else   FRA17032   S.L.C.
       Some more text
     DOC
-
-    expect(Processors::RemoveHeaders.process(document)).to eq(<<-DOC)
+    to eq(<<-DOC)
       Some text
       Something else   FRA17032   S.L.C.
       Some more text
@@ -47,18 +42,35 @@ describe Processors::RemoveHeaders do
   end
 
   it "Only identifies headers of the format AAA#####" do
-    document = <<-DOC
+    expect(Processors::RemoveHeaders.process(<<-DOC)).
       II
       Some text
       Section II
       Some more text
     DOC
-
-    expect(Processors::RemoveHeaders.process(document)).to eq(<<-DOC)
+    to eq(<<-DOC)
       II
       Some text
       Section II
       Some more text
+    DOC
+  end
+
+  it "removes page numbers on lines immediately following the headers" do
+    expect(Processors::RemoveHeaders.process(<<-DOC)).
+      FRA17032      S.L.C.
+      Some text
+      FRA17032      S.L.C.
+              2
+      Some more text
+      FRA17032      S.L.C.
+              3
+      Even more text
+    DOC
+    to eq(<<-DOC)
+      Some text
+      Some more text
+      Even more text
     DOC
   end
 end
